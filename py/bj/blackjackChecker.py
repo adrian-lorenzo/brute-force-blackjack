1956fbd457f6eb3b2b4a0db60b1118bcc25ebfcc
AdrianLorenzoDev <defuzah@gmail.com>
2018-11-16 15:29:23 +0000

    Python program refactorized

--- py/bj/blackjackChecker.py
+++ py/bj/blackjackChecker.py
@@ -1,31 +1,15 @@
-import random
 import time
 from iterator.iterator import iterador
 
-# blackjack - use as control for run the different's modes
-def blackjack(array,CardValue,count,value):
-    if value == 0:
-        array.remove(CardValue)
-        for i in range(1,8):
-            blackjackProb(array,CardValue,i)
-    elif value == 1:
-        blackjackProb(array,0,count)
-    elif value == 2:
-        for i in range(1,8):
-            blackjackProb(array,0,i)
-    else:
-        array.remove(CardValue)
-        blackjackProb(array,CardValue,count)
-
 # blackjackProb - calculates the probability of not passing in blackjack
-def blackjackProb(array,CardValue,count):
-    start = round(time.time() * 1000)
+def blackjackProb(array,cardValue,count):
+    start = time.time()
     wins=0
     plays=0
     a = iterador(len(array),count)
     while a.hasNext():
         hand = a.GetComb()
-        result = CardValue
+        result = cardValue
         for i in hand:
             result = result + array[i]
         if result  <= 21:
@@ -34,6 +18,7 @@ def blackjackProb(array,CardValue,count):
 
     
     porcentaje =  wins/plays * 100
-    print("The probability of not passing in blackjack with a " + str(CardValue)  + 
-    " choosing " + str(count) + " cards is of " + str(round(porcentaje, 2))+ 
-    "%. TIME: "+ str(round(round(time.time() * 1000) - start)) + "ms")
+    print("The probability of not passing in blackjack with a " + str(cardValue)  + 
+    " choosing " + str(count) + " cards is of " + str(round(porcentaje, 6))+ 
+    "%. TIME: "+ str(round((time.time() - start)*1000)) + "ms")
+
--- py/blackjack.py
+++ py/blackjack.py
@@ -1,40 +1,40 @@
 import argparse
-from fileParser.fileReader import fileToArray
-from bj.blackjackChecker import blackjack
-import random
 import logging
 import sys
+from fileParser.fileReader import fileToArray
+from bj.blackjackChecker import blackjackProb
+
+cardValue = 0 # Value of the card selected
+count = 0 # Number of cards to take
 
 # Use of argparse for get the parameters of the command line.
 # We define -n for get the int values , -i for intense mode, -np for nopick mode 
-# and 2 intengers for CardValue and count.
+# and 2 intengers for cardValue and count.
 def getParameters():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i","--intense", help="Use -i for intense mode: return probability from 1 to 8",
+    parser.add_argument("-intense","--intense", help="Use -intense for intense mode: return probability from 1 to 8",
                         action="store_true")
-    parser.add_argument("-np","--nopick", help="Use -np for nopick mode: probability without picking any card",
+    parser.add_argument("-nopick","--nopick", help="Use -nopick for nopick mode: probability without picking any card",
                         action="store_true")
-    parser.add_argument('integers', metavar='N', type=int, nargs='*',
-                         help='an integer')                   
+    parser.add_argument('integers', metavar='Cards', type=int, nargs='*',
+                         help='Number of card selected and/or number of cards to pick')                   
     args = parser.parse_args()
+
     global cardValue
     global count
+
     if args.intense and args.nopick:
-        cardValue = 0
-        count = 0
         return 2
     if args.intense:
         if not args.integers:
             logging.exception("Not enough arguments")
             sys.exit(2)
-        count = 0
         cardValue = args.integers[0]
         return 0
     elif args.nopick:
         if not args.integers:
             logging.exception("Not enough arguments")
             sys.exit(2)
-        cardValue = 0
         count = args.integers[0]
         return 1
     elif len(args.integers) < 2:
@@ -43,11 +43,23 @@ def getParameters():
     cardValue = args.integers[0]
     count = args.integers[1]
 
+# execBlackjack - use as control for run the different's modes
+def execBlackjack(array,value):
+    if value == 0:
+        array.remove(cardValue)
+        for i in range(1,9):
+            blackjackProb(array,cardValue,i)
+    elif value == 1:
+        blackjackProb(array,0,count)
+    elif value == 2:
+        for i in range(1,9):
+            blackjackProb(array,0,i)
+    else:
+        array.remove(cardValue)
+        blackjackProb(array,cardValue,count)
+    sys.exit(0)
+
 if __name__ == '__main__':
     value = getParameters()
     nums = fileToArray("../deck.dat")
-    random.shuffle(nums)
-    blackjack(nums,cardValue,count,value)
-    sys.exit(0)
-
-    
+    execBlackjack(nums,value)
